5037b6b5aef4b5eeaa8c960a58931f88fde213da
IoanaTincu <91495477+IoanaTincu@users.noreply.github.com>
2023-03-24 19:17:01 +0200

    Generate vocabulary

--- DataSet.py
+++ DataSet.py
@@ -1,62 +1,77 @@
 import xml.etree.ElementTree as ET
 import os
 import random
-from nltk.tokenize import word_tokenize
-import nltk
 from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
 import spacy
 from gensim.parsing.preprocessing import STOPWORDS
+from spacy.lang.en import English
 
 
-def parse_XML_file(xmlFile):
-    parsedFile = ET.parse(xmlFile)
-    root = parsedFile.getroot()
+class TextDocumentsProcessing:
 
-    title = root.find('title').text
-    text = '\n'.join([p.text for p in root.find('text').findall('p')])
-    topics = [code.attrib['code'] for code in root.findall(".//codes[@class='bip:topics:1.0']/code")]
-
-    return title, text, topics
-
-
-def generate_files(numberFiles):
-    path = r"C:\Users\Tincu\Downloads\Reuters\Reuters_34\Training"
-
-    xmlFiles = [os.path.join(path, f) for f in os.listdir(path)]
-
-    return random.sample(xmlFiles, numberFiles)
+    def __init__(self):
+        self.nlp = spacy.load('en_core_web_sm')
+        self.spacyStopwords = spacy.lang.en.stop_words.STOP_WORDS
+        self.stopwordsList = set(list(STOPWORDS) + list(self.spacyStopwords) + stopwords.words('english'))
+        self.postags = ['NOUN', 'PROPN', 'VERB', 'ADJ']
+        self.vocabulary = set()
+        self.samples = []
 
 
-def perform_feature_extraction(title, text):
-    tokens = word_tokenize(title + text)
+    def parse_XML_file(self, xmlFile):
+        parsedFile = ET.parse(xmlFile)
+        root = parsedFile.getroot()
 
-    nlp = spacy.load('en_core_web_sm')
-    spacyStopwords = spacy.lang.en.stop_words.STOP_WORDS
+        title = root.find('title').text
+        text = '\n'.join([p.text for p in root.find('text').findall('p')])
+        topics = [code.attrib['code'] for code in root.findall(".//codes[@class='bip:topics:1.0']/code")]
 
-    stopwordsList = set(list(STOPWORDS) + list(spacyStopwords) + list(nlp.Defaults.stop_words) + stopwords.words('english'))
-    if set(spacyStopwords) == set(nlp.Defaults.stop_words): print("equal")
-    else: print("not equal")
-
-    lemmatizer = WordNetLemmatizer()
-
-    lemmatizedTokens = []
-    for token in tokens:
-        token = token.lower()
-        if token not in stopwordsList:
-            token = lemmatizer.lemmatize(token)
-            lemmatizedTokens.append(token)
-
-    return tokens
+        return title, text, topics
 
 
+    def generate_files(self, numberFiles):
+        path = r"C:\Users\Tincu\Downloads\Reuters\Reuters_34\Training"
+
+        xmlFiles = [os.path.join(path, f) for f in os.listdir(path)]
+
+        return random.sample(xmlFiles, numberFiles)
 
 
-xmlFile = generate_files(1)
-print(xmlFile)
+    def perform_feature_extraction(self, title, text):
+        textDocumentTokens = self.nlp(title + '\n' + text)
 
-title, text, topics = parse_XML_file(xmlFile[0])
-print(title)
-print(text)
-print(topics)
-print(perform_feature_extraction(title, text))
+        sample = {}
+
+        for token in textDocumentTokens:
+            if not token.is_punct \
+                    and not token.is_space \
+                    and not token.lower_ in self.stopwordsList \
+                    and token.is_alpha \
+                    and token.pos_ in self.postags:
+                processedToken = token.lemma_.lower()
+
+                self.vocabulary.add(processedToken)
+
+                if processedToken not in sample:
+                    sample[processedToken] = 1
+                else:
+                    sample[processedToken] += 1
+
+        self.samples.append(sample)
+
+
+    def process_text_documents(self, numberFiles):
+        samples = self.generate_files(numberFiles)
+
+        for i in range(numberFiles):
+            title, text, topics = self.parse_XML_file(samples[i])
+
+            self.perform_feature_extraction(title, text)
+
+
+
+
+processing = TextDocumentsProcessing()
+processing.process_text_documents(1)
+print(processing.vocabulary)
+print(processing.samples)
